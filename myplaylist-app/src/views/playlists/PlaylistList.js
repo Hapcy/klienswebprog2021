1fe6d79b6294b6dcd7fead2c09e95908f621762d
tibor.moger <tibor.moger@smartx.eu>
2021-03-25 17:32:44 +0100

    controlled input

--- myplaylist-app/src/views/playlists/PlaylistList.js
+++ myplaylist-app/src/views/playlists/PlaylistList.js
@@ -1,7 +1,9 @@
 import { PlaylistTypes } from '../../domain/playlist';
 import classNames from 'classnames';
+import { useState } from 'react';
 
 export function PlaylistList({ playlists, chosenPlaylist, setChosenPlaylist }) {
+  const [newPlaylistName, setNewPlaylistName] = useState('');
 
   const playlistItems = playlists.map((playlist) => (
     <div
@@ -24,13 +26,22 @@ export function PlaylistList({ playlists, chosenPlaylist, setChosenPlaylist }) {
       <h3>Playlists</h3>
       <div className="ui very relaxed selection list">
         {playlistItems}
-        <div className="item" id="newPlaylist">
+        <form className="item" id="newPlaylist">
           <i className="large green plus middle aligned icon"></i>
           <div className="content">
-            <a className="header">New</a>
-            <div className="description">Create a new playlist</div>
+            <span className="header">New</span>
+            <div className="description">
+              {/* Create a new playlist */}
+              {/* Controlled input */}
+              <input
+                type="text"
+                required
+                value={newPlaylistName}
+                onChange={(e) => setNewPlaylistName(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
